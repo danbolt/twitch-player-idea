e2013120dcea7d2764fe01c5ed3d3cfe63e08c3d
Daniel Savage <daniel@MacBook-Pro-2.local>
2018-08-04 12:24:06 -0700

    Initial commit

---
+++ src/gameplay.js
@@ -0,0 +1,12 @@
+var Gameplay = function () {
+  //
+};
+Gameplay.prototype.create = function() {
+  //
+};
+Gameplay.prototype.update = function() {
+	//
+};
+Gameplay.prototype.shutdown = function() {
+  //
+};
---
+++ src/main.js
@@ -0,0 +1,46 @@
+var Preload = function () {
+	//
+};
+Preload.prototype.init = function() {
+  this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+  this.game.scale.refresh();
+
+  this.game.scale.pageAlignHorizontally = true;
+  this.game.scale.pageAlignVertically = true;
+
+  // enable crisp rendering
+  this.game.stage.smoothed = false;
+  this.game.renderer.renderSession.roundPixels = true;  
+  Phaser.Canvas.setImageRenderingCrisp(this.game.canvas);
+  PIXI.scaleModes.DEFAULT = PIXI.scaleModes.NEAREST; //for WebGL
+
+  this.game.input.keyboard.addKeyCapture(Phaser.Keyboard.DOWN);
+  this.game.input.keyboard.addKeyCapture(Phaser.Keyboard.UP);
+  this.game.input.keyboard.addKeyCapture(Phaser.Keyboard.SPACEBAR);
+
+  this.game.input.gamepad.start();
+};
+Preload.prototype.preload = function() {
+  // Font is Gamegirl Classic by freakyfonts
+  // License is for noncommercial use
+  // http://www.fontspace.com/freaky-fonts/gamegirl-classic
+  this.game.load.bitmapFont('font', 'asset/font/font.png', 'asset/font/font.json');
+
+  // Move these to load later
+  this.game.load.spritesheet('test_sheet', 'asset/image/test.png', 32, 32);
+};
+Preload.prototype.create = function() {
+  this.game.state.start('Gameplay');
+};
+
+
+
+var main = function () {
+	console.log('hello, agbic! 😊');
+
+	var game = new Phaser.Game(640, 480);
+	game.state.add('Preload', Preload, false);
+  game.state.add('Gameplay', Gameplay, false);
+
+	game.state.start('Preload');
+};

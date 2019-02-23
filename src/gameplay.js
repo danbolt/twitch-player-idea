1b35f38af6424159bdc8b532a73efe1975019c33
Daniel Savage <dansvg@gmail.com>
2019-02-22 20:26:50 -0800

    rotation

--- src/gameplay.js
+++ src/gameplay.js
@@ -1,6 +1,6 @@
 
-const PlayerMoveSpeed = 170.0;
-const PlayerBurstSpeed = 370.0;
+const PlayerMoveSpeed = 210.0;
+const PlayerBurstSpeed = 270.0;
 const PlayerBurstDecayTime = 230;
 
 const Epsilon = 0.0001;
@@ -15,6 +15,7 @@ var Player = function(game, x, y) {
   this.data.burstTween = null;
 
   this.game.physics.enable(this, Phaser.Physics.ARCADE);
+  this.anchor.set(0.5, 0.5);
 }
 Player.prototype = Object.create(Phaser.Sprite.prototype);
 Player.prototype.constructor = Player;
@@ -22,6 +23,7 @@ Player.prototype.updateDirectionFromInput = function() {
   this.data.prevMoveDirection.x = this.data.moveDirection.x;
   this.data.prevMoveDirection.y = this.data.moveDirection.y;
 
+  // keyboard
   if (this.game.input.keyboard.isDown(Phaser.KeyCode.RIGHT)) {
     this.data.moveDirection.x = 1.0;
   } else if (this.game.input.keyboard.isDown(Phaser.KeyCode.LEFT)) {
@@ -29,7 +31,6 @@ Player.prototype.updateDirectionFromInput = function() {
   } else {
     this.data.moveDirection.x = 0.0;
   }
-
   if (this.game.input.keyboard.isDown(Phaser.KeyCode.DOWN)) {
     this.data.moveDirection.y = 1.0;
   } else if (this.game.input.keyboard.isDown(Phaser.KeyCode.UP)) {
@@ -43,6 +44,8 @@ Player.prototype.updateDirectionFromInput = function() {
 Player.prototype.updateVelocityFromDirection = function() {
   const prevDirectionLengthSqr = this.data.prevMoveDirection.getMagnitudeSq();
   const currentDirectionLengthSqr = this.data.moveDirection.getMagnitudeSq();
+
+  // Dash a bit when movement starts from a standstill
   if ((prevDirectionLengthSqr <= Epsilon) && (currentDirectionLengthSqr > prevDirectionLengthSqr) && (this.data.burstTween === null)) {
     this.data.moveSpeed = PlayerBurstSpeed;
     this.data.burstTween = this.game.add.tween(this.data);
@@ -53,12 +56,19 @@ Player.prototype.updateVelocityFromDirection = function() {
     this.data.burstTween.start();
   }
 
+  if (currentDirectionLengthSqr > Epsilon) {
+    const targetRotation = Math.atan2(this.data.moveDirection.y, this.data.moveDirection.x);
+    this.rotation = Phaser.Math.linear(this.rotation, targetRotation, 0.4);
+    this.rotation = Phaser.Math.wrapAngle(this.rotation, true);
+  }
+
   this.body.velocity.x = this.data.moveSpeed * this.data.moveDirection.x;
   this.body.velocity.y = this.data.moveSpeed * this.data.moveDirection.y;
 };
 Player.prototype.update = function() {
   this.updateDirectionFromInput();
   this.updateVelocityFromDirection();
+
 };
 
 var Gameplay = function () {
